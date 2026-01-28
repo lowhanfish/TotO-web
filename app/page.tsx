2dcc4d00c6b032c876dde98a87daa06a8ad4d679
lowhanfish <lowhanfish@gmail.com>
2026-01-28 13:45:33 +0800

    fix project structure

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <div>Dashboard Toto-L</div>
+      <div>Public Services page</div>
     </div>
   );
 }
