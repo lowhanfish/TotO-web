21f6049a9e655fd71b0ec63325dcd8b1e1f6f05e
lowhanfish <lowhanfish@gmail.com>
2026-02-03 00:05:12 +0800

    added Key Features

--- app/page.tsx
+++ app/page.tsx
@@ -73,16 +73,37 @@ export default function Home() {
         </div>
 
 
-        <div className="flex gap-3">
-          <div className="flex-1">
-            <Image src="/images/key1.png" alt="" width={1920} height={600} className="w-full h-auto object-cover mt-20" />
-            <p>Smart Document Ingestion (RAG Technology)</p>
-            <p>Simply upload PDF files, DOCX documents, or other technical manuals. TotO-L will “learn” the content within seconds—without the need for model retraining</p>
+        <div className="mt-10 px-8">
+
+          <div className="text-center mb-5">
+            <p>
+              <span className="text-[36px] ">Key</span>
+              <span className="text-[42px] font-bold text-ltwo-6"> Features</span>
+            </p>
           </div>
 
 
-          <div className="flex-1"></div>
-          <div className="flex-1"></div>
+          <div className="flex flex-col md:flex-row gap-10">
+            <div className="flex-1 p-10 bg-ltwo-2/20 text-center border-8 border-lfirst-3 rounded-[24]">
+              <Image src="/images/key1.png" alt="" width={1920} height={600} className="w-full h-auto object-cover" />
+              <p className="text-[20px] font-semibold opacity-70 leading-5 text-ltwo-7 pt-5">Smart Document Ingestion (RAG Technology)</p>
+              <p className="font-light text-[16px] leading-5 pt-5">Simply upload PDF files, DOCX documents, or other technical manuals. TotO-L will “learn” the content within seconds—without the need for model retraining</p>
+            </div>
+            <div className="flex-1 p-10 bg-[#87A1A0] text-center border-8 border-lfirst-5 rounded-[24]">
+              <Image src="/images/key2.png" alt="" width={1920} height={600} className="w-full h-auto object-cover" />
+              <p className="text-[20px] font-semibold opacity-70 leading-5 text-ltwo-1 pt-5">Smart Document Ingestion (RAG Technology)</p>
+              <p className="font-light text-[16px] leading-5 pt-5">Simply upload PDF files, DOCX documents, or other technical manuals. TotO-L will “learn” the content within seconds—without the need for model retraining</p>
+            </div>
+            <div className="flex-1 p-10 bg-ltwo-2/20 text-center border-8 border-lfirst-3 rounded-[24]">
+              <Image src="/images/key3.png" alt="" width={1920} height={600} className="w-full h-auto object-cover" />
+              <p className="text-[20px] font-semibold opacity-70 leading-5 text-ltwo-7 pt-5">Smart Document Ingestion (RAG Technology)</p>
+              <p className="font-light text-[16px] leading-5 pt-5">Simply upload PDF files, DOCX documents, or other technical manuals. TotO-L will “learn” the content within seconds—without the need for model retraining</p>
+            </div>
+
+
+
+          </div>
+
 
 
         </div>
