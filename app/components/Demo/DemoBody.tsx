4d84d78c3f1a36c8c336ae1ea376e0a5228bec6d
lowhanfish <lowhanfish@gmail.com>
2026-02-09 23:05:37 +0800

    split DemoBody

--- app/components/Demo/DemoBody.tsx
+++ app/components/Demo/DemoBody.tsx
@@ -1,39 +1,15 @@
 import React from 'react'
-import { BsFillSendArrowUpFill } from "react-icons/bs";
+import DemoList from './DemoList';
+import DemoContent from './DemoContent';
+
 
 const DemoBody = () => {
     return (
         <div className='flex flex-col lg:flex-row px-2 pt-10 xl:pt-2 xl:px-10 h-full'>
-            <div className='w-90 p-2'>
-                <div className='bg-lfirst-6/15 rounded-[5] h-full'>
-                    bb
-                </div>
-            </div>
+            <DemoList />
             {/* BODY2 */}
-            <div className='w-full p-2 h-full flex flex-col gap-5'>
-                <div className='bg-gray-300 rounded-[5] h-full'>
-                    {/* SUB-BODY2 */}
-                    <div className=' w-full h-full flex flex-col gap-5 justify-center items-center'>
-                        <div className='
-                                bg-gray-200 rounded-[10] border-3 border-lfirst-6 w-[85%] md:w-[70%] fixed bottom-4 h-30 
-                                whitespace-pre-wrap leading-6
-                                '>
-                            <textarea className='w-full text-[14px] text-lfirst-2 p-3 h-[90%] resize-none focus:outline-none'></textarea>
-                            <div className='flex justify-end -mt-8 mr-3'>
-                                <div className='bg-lfirst-2 h-8 w-8 rounded-[50] flex justify-center items-center cursor-pointer border-2 border-lfirst-4'>
-                                    <BsFillSendArrowUpFill className='text-lfirst-6' />
+            <DemoContent />
 
-                                </div>
-                            </div>
-
-                        </div>
-
-                    </div>
-
-
-                </div>
-
-            </div>
         </div>
     )
 }
---
+++ app/components/Demo/DemoList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DemoList = () => {
+    return (
+        <div className='w-90 p-2'>
+            <div className='bg-lfirst-6/15 rounded-[5] h-full'>
+                bb
+            </div>
+        </div>
+    )
+}
+
+export default DemoList
