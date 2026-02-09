98d0536349cc3751bba73f70e9d7ea1f7dd0eb03
lowhanfish <lowhanfish@gmail.com>
2026-02-09 23:01:54 +0800

    split demo page

---
+++ app/components/Demo/DemoBody.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { BsFillSendArrowUpFill } from "react-icons/bs";
+
+const DemoBody = () => {
+    return (
+        <div className='flex flex-col lg:flex-row px-2 pt-10 xl:pt-2 xl:px-10 h-full'>
+            <div className='w-90 p-2'>
+                <div className='bg-lfirst-6/15 rounded-[5] h-full'>
+                    bb
+                </div>
+            </div>
+            {/* BODY2 */}
+            <div className='w-full p-2 h-full flex flex-col gap-5'>
+                <div className='bg-gray-300 rounded-[5] h-full'>
+                    {/* SUB-BODY2 */}
+                    <div className=' w-full h-full flex flex-col gap-5 justify-center items-center'>
+                        <div className='
+                                bg-gray-200 rounded-[10] border-3 border-lfirst-6 w-[85%] md:w-[70%] fixed bottom-4 h-30 
+                                whitespace-pre-wrap leading-6
+                                '>
+                            <textarea className='w-full text-[14px] text-lfirst-2 p-3 h-[90%] resize-none focus:outline-none'></textarea>
+                            <div className='flex justify-end -mt-8 mr-3'>
+                                <div className='bg-lfirst-2 h-8 w-8 rounded-[50] flex justify-center items-center cursor-pointer border-2 border-lfirst-4'>
+                                    <BsFillSendArrowUpFill className='text-lfirst-6' />
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default DemoBody
--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -1,36 +1,12 @@
 import React from 'react'
+import DemoBody from '../components/Demo/DemoBody'
+
+
 
 const page = () => {
     return (
         <div className='w-full h-full'>
-            <div className='flex flex-col lg:flex-row px-2 pt-10 xl:pt-2 xl:px-10 h-full'>
-                <div className='w-90 p-2'>
-                    <div className='bg-lfirst-6/15 rounded-[5] h-full'>
-                        bb
-                    </div>
-                </div>
-                {/* BODY2 */}
-                <div className='w-full p-2 h-full flex flex-col gap-5'>
-                    <div className='bg-lfirst-7 rounded-[5] h-full'>
-
-
-
-
-
-                        {/* SUB-BODY2 */}
-                        <div className=' w-full h-full flex flex-col gap-5 justify-center items-center'>
-                            <div className=' w-[70%] fixed bottom-5 h-30'>
-                                <textarea className='w-full bg-lfirst-6 text-lfirst-2 p-3 h-full rounded-[10] border border-lfirst-5'></textarea>
-
-                            </div>
-
-                        </div>
-
-
-                    </div>
-
-                </div>
-            </div>
+            <DemoBody />
         </div>
     )
 }
